ec5d7973f5c96c3732a7fd242e56e0f1fc2b5ea2
Nizar Lazuardy Firmansyah <nizar.digital@gmail.com>
2012-10-07 17:12:28 +0700

    big update ! work theme chooser, init.d script, resolv.conf, etc

--- pico.mk
+++ pico.mk
@@ -51,14 +51,64 @@ PRODUCT_PACKAGES += \
     
 # Camera
 PRODUCT_PACKAGES += \
-    camera.default    
+    camera.default   
+
+# T-Mobile theme engine
+include vendor/aokp/configs/themes_common.mk 
     
 # Misc
 PRODUCT_PACKAGES += \
-    Superuser \
+    MusicFX \
+    MusicVisualization \
+    NoiseField \
+    ROMControl \
+    PhaseBeam \
+    su \
+    AppWidgetPicker \
+    openvpn \
     SwagPapers \
-    ThemeChooser \
+    AOKPtips \
     com.android.future.usb.accessory
+
+# Blobs common to all devices
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk \
+    vendor/aokp/prebuilt/common/app/LatinImeDictionaryPack.apk:system/app/LatinImeDictionaryPack.apk \
+    vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf  \
+    vendor/aokp/prebuilt/common/app/Microbes.apk:system/app/Microbes.apk \
+    vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
+    vendor/aokp/prebuilt/common/app/UnicornPorn.apk:system/app/UnicornPorn.apk
+
+# init.d
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check \
+    vendor/aokp/prebuilt/common/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
+    vendor/aokp/prebuilt/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
+    vendor/aokp/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+    vendor/aokp/prebuilt/common/etc/init.d/05freemem:system/etc/init.d/05freemem \
+    vendor/aokp/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
+    vendor/aokp/prebuilt/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
+    vendor/aokp/prebuilt/common/etc/init.d/09cron:system/etc/init.d/09cron \
+    vendor/aokp/prebuilt/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
+    vendor/aokp/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
+    vendor/aokp/prebuilt/common/etc/init_trigger.disabled:system/etc/init_trigger.disabled \
+    vendor/aokp/prebuilt/common/etc/liberty.bsh:system/etc/liberty.bsh \
+    vendor/aokp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
+    
+# Cron
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/common/etc/cron/cron.conf:system/etc/cron/cron.conf \
+    vendor/aokp/prebuilt/common/etc/cron/cron.hourly/00drop_caches:system/etc/cron/cron.hourly/00drop_caches \
+    vendor/aokp/prebuilt/common/etc/cron/cron.daily/00drop_caches:system/etc/cron/cron.daily/00drop_caches \
+    vendor/aokp/prebuilt/common/etc/cron/cron.weekly/00drop_caches:system/etc/cron/cron.weekly/00drop_caches \
+    vendor/aokp/prebuilt/common/etc/cron/cron.hourly/01clear_cache:system/etc/cron/cron.hourly/01clear_cache \
+    vendor/aokp/prebuilt/common/etc/cron/cron.daily/01clear_cache:system/etc/cron/cron.daily/01clear_cache \
+    vendor/aokp/prebuilt/common/etc/cron/cron.weekly/01clear_cache:system/etc/cron/cron.weekly/01clear_cache 
+    
+# Adding files needed for extra camera features
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/aokp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
 
 # Hardware properties 
 PRODUCT_COPY_FILES += \
@@ -153,8 +203,7 @@ PRODUCT_COPY_FILES += \
     device/htc/pico/prebuilt/etc/firmware/fw_bcm4330_apsta_b2.bin:system/etc/firmware/fw_bcm4330_apsta_b2.bin \
     device/htc/pico/prebuilt/etc/firmware/fw_bcm4330_p2p_b2.bin:system/etc/firmware/fw_bcm4330_p2p_b2.bin \
     
-# Audio
-# Need all audio filters
+# Audio (Need all audio filters)
 PRODUCT_COPY_FILES += \
     device/htc/pico/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
     device/htc/pico/prebuilt/etc/AudioBTID.csv:system/etc/AudioBTID.csv \
